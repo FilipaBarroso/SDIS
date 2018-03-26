9e5c16a7a6edf02690d29fd50548f2ff7a709418
Filipa Barroso <up201307852@fe.up.pt>
2018-03-26 19:31:55 +0100

    database

---
+++ Proj/src/database/Database.java
@@ -0,0 +1,15 @@
+package database;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Database implements Serializable {
+	
+	private static final long serialVersionUID = 3L;
+	
+	public Database(){
+		
+	}
+	
+}
