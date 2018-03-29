31d460e0445a0f7fcdd1049b60f698cc09ea5e75
Frederico Rocha <fred.p.p.r@gmail.com>
2018-03-29 17:41:48 +0100

    Restored started

---
+++ Proj/src/Proj/src/channels/Messenger.java
@@ -0,0 +1,122 @@
+package channels;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+import protocol.Protocol;
+import service.Peer;
+import service.PeerService;
+
+/*
+ * Formats and sends messages
+ */
+public class Messenger implements Runnable {
+
+	private static byte[] buffer;
+	private static BufferedReader cin;
+	private static MulticastSocket socket;
+	private static Peer localPeer;
+	private static InetAddress server;
+
+	// TODO call this with all channel addrs and ports
+	public Messenger(MulticastSocket socket, Peer localPeer, InetAddress server) throws Exception {
+		buffer = new byte[1024];
+		cin = new BufferedReader(new InputStreamReader(System.in));
+
+		this.socket = socket;
+		this.server = server;
+
+		System.out.println("MESSENGER: Login from peer " + localPeer.get_ip() + ":" + localPeer.get_port());
+		System.out.println("MESSENGER: Valid Operations: backup\trestore\t");
+	}
+
+	public void run() {
+		while(true) { try {
+
+			String s = (String)cin.readLine();
+			byte[] msg_data = s.getBytes();
+			
+			// read the console and apply the desired protocol
+			int c = decypherConsole(s);
+			switch(c) {
+			case 1:
+				handleBackupMsg();
+				break;
+			case 2:
+				handleRestoreMsg();
+				break;
+			default:
+				break;
+			}
+			
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		}
+	}
+
+	public static void handleBackupMsg() {
+		try {
+			String filename;
+			int repD;
+		
+			System.out.println("MESSENGER: Specify backup <filename replicationDegree>");
+			String s = (String)cin.readLine();
+			String[] tokens = s.split("\\s+");
+			
+			filename = tokens[0];
+			repD = Integer.parseInt(tokens[1]);
+			
+			Protocol.initiateBackup(filename, repD);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void handleRestoreMsg() {
+		try {
+			String filename;
+			int repD;
+		
+			System.out.println("MESSENGER: Specify restore <filename>");
+			String s = (String)cin.readLine();
+			String[] tokens = s.split("\\s+");
+			
+			filename = tokens[0];
+			repD = Integer.parseInt(tokens[1]);
+			
+			Protocol.initiateBackup(filename, repD);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static int decypherConsole(String s) {
+		if(s.equals("backup")) return 1;
+		if(s.equals("restore")) return 2;
+		if(s.equals("delete")) return 3;
+		
+		
+		return 0;
+	}
+	
+	public static void sendToMC(byte[] msg) throws Exception {
+		DatagramPacket mc_packet = new DatagramPacket(msg, msg.length, server, PeerService.default_MCport);
+		socket.send(mc_packet);
+	}
+	
+	public static void sendToMDB(byte[] msg) throws Exception {
+		DatagramPacket mdb_packet = new DatagramPacket(msg, msg.length, server, PeerService.default_MDBport);
+		socket.send(mdb_packet);
+	}
+}
---
+++ Proj/src/Proj/src/channels/MulticastChannel.java
@@ -0,0 +1,124 @@
+package channels;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.Random;
+
+import protocol.Protocol;
+import service.Peer;
+
+public abstract class MulticastChannel {
+
+	private InetAddress ip;
+	private int port;
+
+	public MulticastChannel(InetAddress ip, int port) {
+		this.setIp(ip);
+		this.setPort(port);
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public InetAddress getIp() {
+		return ip;
+	}
+
+	public void setIp(InetAddress ip) {
+		this.ip = ip;
+	}
+
+	// SHA256 hash function
+	public static final String sha256(String str) {
+		try {
+			MessageDigest sha = MessageDigest.getInstance("SHA-256");
+
+			byte[] hash = sha.digest(str.getBytes(StandardCharsets.UTF_8));
+
+			StringBuffer hexStringBuffer = new StringBuffer();
+
+			for (int i = 0; i < hash.length; i++) {
+				String hex = Integer.toHexString(0xff & hash[i]);
+
+				if (hex.length() == 1)
+					hexStringBuffer.append('0');
+
+				hexStringBuffer.append(hex);
+			}
+
+			return hexStringBuffer.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+
+	public static String[] msgTokens;
+
+	public static void decypherMsg(byte[] msg, Peer sender) {
+		// extract header
+		String s = new String(msg, 0, msg.length);
+		msgTokens = s.split("\\s+");
+
+		switch(msgTokens[0]) {
+		case "PUTCHUNK":
+			Protocol.handlePUTCHUNK(msg, sender);
+			break;
+		case "STORED":
+			Protocol.handleSTORED(msg, sender);
+			break;
+		case "GETCHUNK":
+			Protocol.handleGETCHUNK(msg, sender);
+			break;
+		case "CHUNK":
+			Protocol.handleCHUNK(msg, sender);
+			break;
+		case "DELETE":
+			break;
+		default:
+			break;
+		}
+	}
+
+	public static byte[] extractBody(byte[] msg) throws IOException {
+		ByteArrayInputStream stream = new ByteArrayInputStream(msg);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+		// ler a msg até encontrar o CRLF
+		String line = null;
+		int headerBytes = 0;
+		do {
+			line = reader.readLine();
+			headerBytes += line.getBytes().length;
+		} while(!line.isEmpty());
+
+		headerBytes += 2*Protocol.CRLF.getBytes().length;
+		byte[] body = new byte[msg.length - headerBytes];
+		System.arraycopy(msg, headerBytes, body, 0, msg.length - headerBytes);
+
+		return body;
+	}
+
+	public static byte[] readyPacket(byte[] header, byte[] data) {
+		byte[] packet = new byte[header.length + data.length];
+
+		System.arraycopy(header, 0, packet, 0, header.length);
+		System.arraycopy(data, 0, packet, header.length, data.length);
+
+		return packet;
+	}
+
+}
