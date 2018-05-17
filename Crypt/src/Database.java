afe5e8ab08db435f06a93eb879bc2b9d861aa66f
Frederico Rocha <fred.p.p.r@gmail.com>
2018-05-17 10:12:11 +0100

    Basis for peer2peer

---
+++ Crypt/src/Database.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.security.MessageDigest;
+import java.util.Random;
+
+/*
+ * maintains the blockchain stored
+ * check last proj's database
+ */
+public class Database {
+	
+	public Database() {
+		
+	}
+}
---
+++ Crypt/src/User.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.security.MessageDigest;
+import java.util.Random;
+
+/*
+ * connect to multicast channel thread
+ * ask for user input regarding transactions, checking wallet balance etc..
+ */
+public class User {
+	
+	public User() {
+		
+	}
+}
